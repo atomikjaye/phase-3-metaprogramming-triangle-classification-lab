140113fb3dce01d692ac139f85f84924a7c7ba77
Jocelyn Naarden <tzionone@gmail.com>
2022-07-20 00:58:42 -0400

    Finished Assignment ✔

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,35 @@
 class Triangle
+  attr_reader :side1, :side2, :side2
   # write code here
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+
+  def kind
+    is_valid_triangle
+    if @side1 == @side2 && @side2 == @side3
+      :equilateral
+    elsif @side1 == @side2 || @side1 == @side3 || @side2 == @side3
+      :isosceles
+    # elsif @side1 != @side2 && @side1 != @side3 && @side2 != @side2
+    else
+      :scalene
+      # raise TriangleError
+    end
+  end
+
+  def is_valid_triangle
+    # make sure all @sides do not equal 0
+    is_valid = (@side1 > 0 && @side2 > 0 && @side3 > 0) && 
+    ((@side1 + @side2 > @side3) && (@side2 + @side3 > @side1) && (@side3 + @side1 > @side2) ) 
+    # makse sure two @sides added > third side
+    raise TriangleError unless is_valid
+  end 
+
+
+
+  class TriangleError < StandardError
+  end
 end
